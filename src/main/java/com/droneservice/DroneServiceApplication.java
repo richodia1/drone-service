1815c25804b0413532a8e51f0e4a95b4301239d7
Richard Okosodo <okosodorichard@yahoo.com>
2023-12-29 21:46:56 -0600

    first commit

---
+++ src/main/java/com/droneservice/DroneServiceApplication.java
@@ -0,0 +1,13 @@
+package com.droneservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneServiceApplication.class, args);
+	}
+
+}
